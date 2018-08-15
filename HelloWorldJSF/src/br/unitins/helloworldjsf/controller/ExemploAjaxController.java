fb4af0781290d61f778e016f8f0c2f01fb15be85
Aluno <Aluno@PAT1725>
2018-08-15 18:28:49 -0300

    exemplo Ajax

---
+++ HelloWorldJSF/src/br/unitins/helloworldjsf/controller/ExemploAjaxController.java
@@ -0,0 +1,27 @@
+package br.unitins.helloworldjsf.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class ExemploAjaxController implements Serializable {
+
+	private static final long serialVersionUID = 865560632144603636L;
+
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+		
+}
